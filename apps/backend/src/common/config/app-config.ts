3f6876f24861a7d8bc590b7bd166d56203e72b0a
Michal Molenda <michalus88@gmail.com>
2026-06-19 11:13:16 +0200

    chore: add non-null assertion to AppConfig properties

--- apps/backend/src/common/config/app-config.ts
+++ apps/backend/src/common/config/app-config.ts
@@ -3,14 +3,14 @@ import { IsInt, IsNotEmpty, IsString, IsUrl, Max, Min } from 'class-validator';
 export class AppConfig {
   @IsString()
   @IsNotEmpty()
-  DATABASE_URL: string;
+  DATABASE_URL!: string;
 
   @IsUrl({ require_tld: false })
   @IsNotEmpty()
-  CORS_ORIGIN: string;
+  CORS_ORIGIN!: string;
 
   @IsInt()
   @Min(1)
   @Max(65535)
-  PORT: number;
+  PORT!: number;
 }
